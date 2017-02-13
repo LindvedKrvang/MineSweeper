04b9d348794d7e37f91c2780289e3e7a11f1e396
LindvedKrvang <rklindved@gmail.com>
2017-02-13 20:52:07 +0100

    Inital framework

---
+++ src/minesweeper/gui/controller/GameBoardViewController.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minesweeper.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ListChangeListener;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Rasmus
+ */
+public class GameBoardViewController implements Initializable, ListChangeListener<Object> {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void onChanged(Change<? extends Object> c) {
+        
+    }
+    
+}
